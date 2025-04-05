369dee1d596fa74eebd90bfb05936cd95cac934f
Adeline <adeline.patenne@hotmail.fr>
2025-04-05 17:28:45 +0200

    feat: use of fopen function and start writing and reading file with c

---
+++ day04/file_management.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file_management.h"
+
+int main(int argc, char const *argv[])
+{
+    FILE * file = NULL;
+    file = fopen(FILE_PATH, "r+");
+    int age = 0;
+    char name[100] = "";
+    int actual_char = 0;
+    char string[SIZE_MAX_ARRAY] = "";
+
+    printf("\nvaleur du pointeur file : %p\n", file);
+
+    if(file != NULL)
+    {
+        printf("Ouverture du fichier avec succès.");
+        
+        fputs("Mouhahaha!!!\nJ'en ai tellement marre de cette formation...\nJe veux des vacances bien longues là.\n\n", file);
+        fputc('A', file);
+
+        printf("Votre nom ?\n");
+        scanf("%s", &name);
+        printf("Quel âge avez vous ?\n");
+        scanf("%d", &age);
+
+        fprintf(file, "Vous êtes %s et vous avez %d ans", name, age);
+        fclose(file);
+    }
+    else
+    {
+        printf("Echec de l'ouverture du fichier.");
+    }
+
+    file = fopen(FILE_PATH, "r");
+
+    if (file != NULL)
+    {
+        // do 
+        // {
+        //     actual_char = fgetc(file);
+        //     printf("%c", actual_char);
+        // } while (actual_char != EOF);
+
+        printf("\n\nTEST DE FGETS\n\n");
+        do
+        {
+            printf("test de string : %s", string);
+        } while (fgets(string, SIZE_MAX_ARRAY, file) != NULL);
+        
+    }
+
+    
+
+    printf("\nvaleur du pointeur file : %p\n", file);
+
+    return 0;
+}
---
+++ day04/file_management.h
@@ -0,0 +1,6 @@
+#ifndef FILE_MANAGEMENT
+#define FILE_MANAGEMENT
+#define FILE_PATH "test.txt"
+#define SIZE_MAX_ARRAY 1000
+
+#endif
